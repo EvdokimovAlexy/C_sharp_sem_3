d6a5f7bfe67e4569ee8744993a600d8aea241246
Prometheus <poligraphich@gmail.com>
2022-09-06 21:10:15 +0300

    add 002_03

---
+++ example002_sem03/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+ Console.WriteLine("Input Quadrant number");
+int quadrant = Convert.ToInt32(Console.ReadLine());
+void GetDiapazon(int quadrant)
+{
+    switch(quadrant)
+    {
+        case 1:
+            Console.WriteLine("допустимый диапазон координат точек в этом квадранте x>0, y>0");
+            break;
+
+        case 2:
+            Console.WriteLine("допустимый диапазон координат точек в этом квадранте x<0, y>0");
+            break;
+
+        case 3:
+            Console.WriteLine("допустимый диапазон координат точек в этом квадранте x<0, y<0");
+            break;
+
+        case 4:
+            Console.WriteLine("допустимый диапазон координат точек в этом квадранте x>0, y<0");
+            break;
+    }
+}
+GetDiapazon(quadrant);
